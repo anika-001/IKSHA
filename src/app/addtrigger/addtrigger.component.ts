4b6a25600368be4ca369fe9538b8cf8636e30d1c
Vaishnavi <vaishnavisdesai@gmail.com>
2021-05-01 19:50:48 +0530

    ...

--- src/app/addtrigger/addtrigger.component.ts
+++ src/app/addtrigger/addtrigger.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-addtrigger',
@@ -7,7 +8,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddtriggerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
+
+  type: string;
+
+  addtrig = this.fb.group({
+    ExpenseName: [''],
+    ExpenseCategory: [''],
+    Amount: ['']
+  })
+
+  catoptions = [
+    {
+      name: "1",
+      displayname: "Travel"
+    },
+    {
+      name: "2",
+      displayname: "Food"
+    },
+    {
+      name: "3",
+      displayname: "Personal" 
+    },
+    {
+      name: "4",
+      displayname: "Health"
+    },
+    {
+      name: "5",
+      displayname: "Subscriptions and Bills"
+    },
+    {
+      name: "6",
+      displayname: "Savings"
+    },
+    {
+      name: "7",
+      displayname: "Loans" 
+    },
+    {
+      name: "8",
+      displayname: "Miscellaneous" 
+    },
+    {
+      name: "9",
+      displayname: "Total" 
+    }
+  ]
 
   ngOnInit(): void {
   }
