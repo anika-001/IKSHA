51404741f4bac5b37e3d6bc05cb172f2f02ce289
Vaishnavi <vaishnavisdesai@gmail.com>
2021-05-02 14:28:56 +0530

    ...

--- src/app/management/management.component.ts
+++ src/app/management/management.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { FormControl, FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { AuthService } from '../Services/auth.service';
 
 @Component({
   selector: 'app-management',
@@ -8,13 +10,13 @@ import { FormControl, FormGroup, FormBuilder, Validators, FormArray } from '@ang
 })
 export class ManagementComponent implements OnInit {
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private as: AuthService, private db: AngularFirestore) { }
   days: any[] = [];
   week=["Sat","Sun","Mon","Tue","Wed","Thur","Fri"];
   showform: boolean=false;
   todoshow: boolean=false;
   type: string;
-  addexp = this.fb.group({
+  addactorrem = this.fb.group({
     ActivityName: [''],
     ActivityTime: ['']
   })
@@ -33,7 +35,17 @@ export class ManagementComponent implements OnInit {
     }
   ]
 
+  userID: any;
+
   ngOnInit(): void {
+
+    this.as.getUserState()
+      .subscribe(user => {
+        this.userID = user.uid;
+        //console.log(farm, id);
+      })
+
+
     for(let x=0; x<31; x++){
        this.days.push({"date": x + 1, "day":this.week[x%7]});
     }
@@ -46,4 +58,17 @@ export class ManagementComponent implements OnInit {
     this.todoshow = val;
   }
 
+  submitactrem(){
+    let data = this.addactorrem.value;
+    console.log(data);
+    data["type"] = this.type;
+    data["date"] = "12 May 2021"
+    this.toggle(false);
+    this.db.collection("Management").doc(this.userID).collection(this.type).add(data).then(res => {
+      console.log(res);
+    })
+    .catch(e => {
+
+    })
+  }
 }
