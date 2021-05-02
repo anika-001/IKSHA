159d71035cd02912139bf5a1718dbc8cb358e096
amisha <amisha.chaudhari21@gmail.com>
2021-05-02 18:06:36 +0530

    amisha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -41,11 +41,9 @@ import { TeamupformComponent } from './teamupform/teamupform.component';
 //import { ChatroomComponent } from './chatroom/chatroom.component';
 import { ResumeComponent } from './resume/resume.component';
 import { ChatComponent } from './chat/chat.component';
-<<<<<<< HEAD
 import { ViewscheduleComponent } from './viewschedule/viewschedule.component';
-=======
 import { AddquestionComponent } from './addquestion/addquestion.component';
->>>>>>> 074473a169f2c0e74441278351e26c85fb15c041
+import { ResourceformComponent } from './resourceform/resourceform.component';
 
 
 firebase.initializeApp(environment.firebaseConfig);
@@ -82,11 +80,9 @@ export class HammerConfig extends HammerGestureConfig {
     TeamupformComponent,
     ResumeComponent,
     ChatComponent,
-<<<<<<< HEAD
-    ViewscheduleComponent
-=======
-    AddquestionComponent
->>>>>>> 074473a169f2c0e74441278351e26c85fb15c041
+    ViewscheduleComponent,
+    AddquestionComponent,
+    ResourceformComponent
   ],
   imports: [
     BrowserModule,
