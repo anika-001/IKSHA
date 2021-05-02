70ad7f0649bf7c29469cb45630248b8760e6f72f
amisha <amisha.chaudhari21@gmail.com>
2021-05-02 20:21:25 +0530

    amisha

--- src/app/finance/finance.component.ts
+++ src/app/finance/finance.component.ts
@@ -10,22 +10,28 @@ export class FinanceComponent implements OnInit {
   constructor() { }
 
   chartdata = [{
-    Label: ['Sangli', 'Pune', 'Nagpur', 'Mumbai'],
-    Data: [80, 160, 200, 100],
-    Type:'doughnut'
+    Label: ['Travel', 'Food', 'Personal', 'Health', 'Subscriptions & bills', 'Savings', 'Loans', 'Miscellanous'],
+    Data: [7, 19, 15, 25, 20, 4, 6, 4],
+    Type:'doughnut', 
+    Name: 'Category wise monthly distribution'
   },
   {
-    Label: ['Sales Q1', 'Sales Q2', 'Sales Q3', 'Sales Q4'],
-    Data: [120, 150, 180, 90],
-    Type: 'pie'
+    Label: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
+    Data: [120, 150, 180, 90, 50],
+    Type: 'pie',
+    Name: 'Savings per month'
   },
   {
-    Labels: ['Potatoes', 'Tomatoes', 'Onions', 'Banana', 'Apples', 'Oranges', 'Papaya'],
+    Labels: ['T', 'F', 'P', 'H', 'S & B', 'S', 'L', 'M'],
     Type: 'bar',
     Data: [
-      {data: [65, 59, 80, 81, 56, 55, 40], label: 'Market Rate'},
-      {data: [28, 48, 40, 19, 86, 27, 90], label: 'Your Rate'}
+      {data: [65, 59, 80, 71, 56, 55, 40, 23], label: 'Jan'},
+      {data: [28, 48, 40, 19, 76, 27, 80, 17], label: 'Feb'},
+      {data: [21, 78, 64, 75, 26, 42, 40, 13], label: 'Mar'},
+      {data: [58, 69, 40, 9, 26, 27, 12, 14], label: 'Apr'},
+      {data: [47, 40, 59, 59, 63, 29, 67, 22], label: 'May'},
     ],
+    Name: 'Expense Comparision for different months',
     Legend: true,
     Options: {
       scaleShowVerticalLines: false,
